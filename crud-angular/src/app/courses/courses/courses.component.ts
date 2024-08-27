4b088b7dd5fdc1785e4aa2420e673b2357d32328
Luyz-Castelo <luyzcastelobranco@hotmail.com>
2024-08-27 13:21:41 +0000

    Criação de uma table para o courses component e de um módulo base para os componentes do material

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    {
+      _id: '1',
+      name: 'Java Básico',
+      category: 'Lógica',
+    },
+    {
+      _id: '2',
+      name: 'Typescript Básico',
+      category: 'FrontEnd',
+    },
+  ];
+  displayedColumns = ['name', 'category'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
